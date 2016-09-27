759ea5292eb58757b04535f07fac0e026d783284
tianmingzhao <83247083@qq.com>
2016-09-27 15:25:54 +0800

    ResultUtil

---
+++ src/main/java/com/quxin/freshfun/common/Constant.java
@@ -0,0 +1,8 @@
+package com.quxin.freshfun.common;
+
+/**
+ * Created by tianmingzhao on 16/9/27.
+ */
+public class Constant {
+
+}
---
+++ src/main/java/com/quxin/freshfun/common/GoodsConstant.java
@@ -0,0 +1,9 @@
+package com.quxin.freshfun.common;
+
+/**
+ * Created by tianmingzhao on 16/9/27.
+ */
+public class GoodsConstant {
+    /** 上架 */
+    public static final Integer GOODS_IS_ONSALE = 1;
+}
