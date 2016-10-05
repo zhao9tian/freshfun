3e4577ffa6e114e69241056436b946710a5827ac
Tu ZL <18721394619@163.com>
2016-10-06 00:16:50 +0800

    页面传参加引号的问题

--- src/main/java/com/quxin/freshfun/controller/withdraw/WithdrawController.java
+++ src/main/java/com/quxin/freshfun/controller/withdraw/WithdrawController.java
@@ -45,16 +45,17 @@ public class WithdrawController {
     public Map<String , Object>  getAllMoneyB(String userId){
         Map<String , Object> resultMap = new HashMap<String , Object>();
         if(userId != null && !"".equals(userId)){
-            Double totalMoney = withdrawService.queryTotalMoneyB(userId);
-            Double unrecordMoney = withdrawService.queryUnrecordMoneyB(userId);
-            Double withdrawCash = withdrawService.queryWithdrawCashB(userId);
+            String uId = userId.replace("\"","");
+            Double totalMoney = withdrawService.queryTotalMoneyB(uId);
+            Double unrecordMoney = withdrawService.queryUnrecordMoneyB(uId);
+            Double withdrawCash = withdrawService.queryWithdrawCashB(uId);
             DecimalFormat df = new DecimalFormat("#0.00");
             resultMap.put("totalMoney" , df.format(totalMoney));
             resultMap.put("unrecordMoney" , df.format(unrecordMoney));
             resultMap.put("withdrawCash" , df.format(withdrawCash));
             resultMap = ResultUtil.success(resultMap);
         }else{
-            ResultUtil.fail(1004,"用户Id不能为空");
+            resultMap = ResultUtil.fail(1004,"用户Id不能为空");
         }
         return resultMap;
     }
@@ -79,7 +80,7 @@ public class WithdrawController {
             resultMap.put("withdrawCash" , df.format(withdrawCash));
             resultMap = ResultUtil.success(resultMap);
         }else{
-            ResultUtil.fail(1004,"用户Id不能为空");
+            resultMap = ResultUtil.fail(1004,"用户Id不能为空");
         }
         return resultMap;
     }
@@ -92,35 +93,39 @@ public class WithdrawController {
     @RequestMapping(value="/applyWithdrawB",method={RequestMethod.POST})
     @ResponseBody
     public Map<String,Object> applyWithdrawB(@RequestBody WithdrawParam extractMoney) {
-        Map<String, Object> resultmap;
-        Double extractmoney = withdrawService.queryWithdrawCashB(extractMoney.getUserId());
-        String userId = extractMoney.getUserId();
-        String extractMoneyStr = extractMoney.getMoney();
-
-        if(userId == null){
-            resultmap = ResultUtil.fail(1004,"userId 为空");
-        }else if(extractmoney == null){
-            resultmap = ResultUtil.fail(1004,"该用户提现金额为0");
-        }else if(extractMoneyStr == null){
-            resultmap = ResultUtil.fail(1004,"提现金额必须大于0");
-        }else if(Double.parseDouble(extractMoneyStr) > extractmoney){
-            resultmap = ResultUtil.fail(1004,"提现金额不能大于可提现金额");
-        }else{
-            WithdrawPOJO withdrawPOJO = new WithdrawPOJO();
-            withdrawPOJO.setCreateDate(System.currentTimeMillis()/1000);
-            withdrawPOJO.setUserId(Long.parseLong(extractMoney.getUserId()));
-            withdrawPOJO.setWithDrawType(extractMoney.getPayway());
-            withdrawPOJO.setPaymentAccount(extractMoney.getAccount());
-            withdrawPOJO.setWithDrawPrice((long)(Double.parseDouble(extractMoney.getMoney())*100));
-            withdrawPOJO.setWithdrawSource(20);
-            Integer record = withdrawService.addWithdraw(withdrawPOJO);
-            if(record == null){
-                resultmap = ResultUtil.fail(1004,"申请提现失败");
+        Map<String, Object> resultMap = null;
+        if(extractMoney.getUserId() != null && !"".equals(extractMoney.getUserId())){
+            String uId = extractMoney.getUserId().replace("\"","");
+            Double extractmoney = withdrawService.queryWithdrawCashB(uId);
+            String userId = uId;
+            String extractMoneyStr = extractMoney.getMoney();
+            if(userId == null){
+                resultMap = ResultUtil.fail(1004,"userId 为空");
+            }else if(extractmoney == null){
+                resultMap = ResultUtil.fail(1004,"该用户提现金额为0");
+            }else if(extractMoneyStr == null){
+                resultMap = ResultUtil.fail(1004,"提现金额必须大于0");
+            }else if(Double.parseDouble(extractMoneyStr) > extractmoney){
+                resultMap = ResultUtil.fail(1004,"提现金额不能大于可提现金额");
             }else{
-                resultmap = ResultUtil.success(1);
+                WithdrawPOJO withdrawPOJO = new WithdrawPOJO();
+                withdrawPOJO.setCreateDate(System.currentTimeMillis()/1000);
+                withdrawPOJO.setUserId(Long.parseLong(extractMoney.getUserId()));
+                withdrawPOJO.setWithDrawType(extractMoney.getPayway());
+                withdrawPOJO.setPaymentAccount(extractMoney.getAccount());
+                withdrawPOJO.setWithDrawPrice((long)(Double.parseDouble(extractMoney.getMoney())*100));
+                withdrawPOJO.setWithdrawSource(20);
+                Integer record = withdrawService.addWithdraw(withdrawPOJO);
+                if(record == null){
+                    resultMap = ResultUtil.fail(1004,"申请提现失败");
+                }else{
+                    resultMap = ResultUtil.success(1);
+                }
             }
+        }else{
+            resultMap = ResultUtil.fail(1004,"用户Id不能为空");
         }
-        return resultmap;
+        return resultMap;
     }
     /**
      * c端申请提现
@@ -130,35 +135,38 @@ public class WithdrawController {
     @RequestMapping(value="/applyWithdrawC",method={RequestMethod.POST})
     @ResponseBody
     public Map<String,Object> applyWithdrawC(@RequestBody WithdrawParam extractMoney) {
-        Map<String, Object> resultmap;
-        String uId = extractMoney.getUserId().replace("\"","");
-        Double extractmoney = withdrawService.queryWithdrawCashC(uId);
-        String extractMoneyStr = extractMoney.getMoney();
-
-        if(uId == null){
-            resultmap = ResultUtil.fail(1004,"userId 为空");
-        }else if(extractmoney == null){
-            resultmap = ResultUtil.fail(1004,"该用户提现金额为0");
-        }else if(extractMoneyStr == null){
-            resultmap = ResultUtil.fail(1004,"提现金额必须大于0");
-        }else if(Double.parseDouble(extractMoneyStr) > extractmoney){
-            resultmap = ResultUtil.fail(1004,"提现金额不能大于可提现金额");
-        }else{
-            WithdrawPOJO withdrawPOJO = new WithdrawPOJO();
-            withdrawPOJO.setCreateDate(System.currentTimeMillis()/1000);
-            withdrawPOJO.setUserId(Long.parseLong(uId));
-            withdrawPOJO.setWithDrawType(extractMoney.getPayway());
-            withdrawPOJO.setPaymentAccount(extractMoney.getAccount());
-            withdrawPOJO.setWithDrawPrice((long)(Double.parseDouble(extractMoney.getMoney())*100));
-            withdrawPOJO.setWithdrawSource(10);
-            Integer record = withdrawService.addWithdraw(withdrawPOJO);
-            if(record == null){
-                resultmap = ResultUtil.fail(1004,"申请提现失败");
+        Map<String, Object> resultMap = null;
+        if(extractMoney.getUserId() != null && !"".equals(extractMoney.getUserId())) {
+            String uId = extractMoney.getUserId().replace("\"", "");
+            Double extractmoney = withdrawService.queryWithdrawCashC(uId);
+            String extractMoneyStr = extractMoney.getMoney();
+            if(uId == null){
+                resultMap = ResultUtil.fail(1004,"userId 为空");
+            }else if(extractmoney == null){
+                resultMap = ResultUtil.fail(1004,"该用户提现金额为0");
+            }else if(extractMoneyStr == null){
+                resultMap = ResultUtil.fail(1004,"提现金额必须大于0");
+            }else if(Double.parseDouble(extractMoneyStr) > extractmoney){
+                resultMap = ResultUtil.fail(1004,"提现金额不能大于可提现金额");
             }else{
-                resultmap = ResultUtil.success(1);
+                WithdrawPOJO withdrawPOJO = new WithdrawPOJO();
+                withdrawPOJO.setCreateDate(System.currentTimeMillis()/1000);
+                withdrawPOJO.setUserId(Long.parseLong(uId));
+                withdrawPOJO.setWithDrawType(extractMoney.getPayway());
+                withdrawPOJO.setPaymentAccount(extractMoney.getAccount());
+                withdrawPOJO.setWithDrawPrice((long)(Double.parseDouble(extractMoney.getMoney())*100));
+                withdrawPOJO.setWithdrawSource(10);
+                Integer record = withdrawService.addWithdraw(withdrawPOJO);
+                if(record == null){
+                    resultMap = ResultUtil.fail(1004,"申请提现失败");
+                }else{
+                    resultMap = ResultUtil.success(1);
+                }
             }
+        }else{
+            resultMap = ResultUtil.fail(1004,"用户Id不能为空");
         }
-        return resultmap;
+        return resultMap;
     }
 
     /**
@@ -168,33 +176,39 @@ public class WithdrawController {
     @RequestMapping(value = "/inOutDetails" , method = RequestMethod.GET)
     @ResponseBody
     public Map<String , Object> inOutDetails(String userId){
-        List<InOutDetailsPOJO> inOutDetailsPOJOs  = withdrawService.queryIncomeRecords(userId);
-        List<InOutDetailsPOJO> withdraws = withdrawService.queryWithdrawRecord(userId);
-        List<InOutDetailsOutParam> allInOut = new ArrayList<>();
+        Map<String , Object> resultMap = null;
+        if(userId != null && !"".equals(userId)){
+            String uId = userId.replace("\"","");
+            List<InOutDetailsPOJO> inOutDetailsPOJOs  = withdrawService.queryIncomeRecords(uId);
+            List<InOutDetailsPOJO> withdraws = withdrawService.queryWithdrawRecord(uId);
+            List<InOutDetailsOutParam> allInOut = new ArrayList<>();
 
-        /** 将两个list整在一起 处理金额/100*/
-        for(InOutDetailsPOJO inOutDetailsPOJO : withdraws ){
-            InOutDetailsOutParam inOutDetailsOutParam = new InOutDetailsOutParam();
-            inOutDetailsOutParam.setGoodsName("提现");//操作
-            inOutDetailsOutParam.setTime(inOutDetailsPOJO.getTime());//时间戳
-            DecimalFormat df = new DecimalFormat("#0.00");
-            inOutDetailsOutParam.setPrice("-"+df.format(((double)inOutDetailsPOJO.getPrice())/100));//金额
-            allInOut.add(inOutDetailsOutParam);
+            /** 将两个list整在一起 处理金额/100*/
+            for(InOutDetailsPOJO inOutDetailsPOJO : withdraws ){
+                InOutDetailsOutParam inOutDetailsOutParam = new InOutDetailsOutParam();
+                inOutDetailsOutParam.setGoodsName("提现");//操作
+                inOutDetailsOutParam.setTime(inOutDetailsPOJO.getTime());//时间戳
+                DecimalFormat df = new DecimalFormat("#0.00");
+                inOutDetailsOutParam.setPrice("-"+df.format(((double)inOutDetailsPOJO.getPrice())/100));//金额
+                allInOut.add(inOutDetailsOutParam);
+            }
+            for(InOutDetailsPOJO inOutDetailsPOJO : inOutDetailsPOJOs ){
+                InOutDetailsOutParam inOutDetailsOutParam = new InOutDetailsOutParam();
+                inOutDetailsOutParam.setGoodsName(inOutDetailsPOJO.getGoodsName());//操作
+                inOutDetailsOutParam.setTime(inOutDetailsPOJO.getTime());
+                DecimalFormat df = new DecimalFormat("#0.00");
+                inOutDetailsOutParam.setPrice(df.format(((double)inOutDetailsPOJO.getPrice())/100));
+                allInOut.add(inOutDetailsOutParam);
+            }
+            //处理页面显示数据
+            //list按时间戳排序
+            ListSortUtil<InOutDetailsOutParam> sortList = new ListSortUtil<InOutDetailsOutParam>();
+            sortList.sort(allInOut , "time" , "desc");
+            resultMap = ResultUtil.success(allInOut);
+        }else{
+            resultMap = ResultUtil.fail(1004,"用户Id不能为空");
         }
-        for(InOutDetailsPOJO inOutDetailsPOJO : inOutDetailsPOJOs ){
-            InOutDetailsOutParam inOutDetailsOutParam = new InOutDetailsOutParam();
-            inOutDetailsOutParam.setGoodsName(inOutDetailsPOJO.getGoodsName());//操作
-            inOutDetailsOutParam.setTime(inOutDetailsPOJO.getTime());
-            DecimalFormat df = new DecimalFormat("#0.00");
-            inOutDetailsOutParam.setPrice(df.format(((double)inOutDetailsPOJO.getPrice())/100));
-            allInOut.add(inOutDetailsOutParam);
-        }
-        //处理页面显示数据
-        //list按时间戳排序
-        ListSortUtil<InOutDetailsOutParam> sortList = new ListSortUtil<InOutDetailsOutParam>();
-        sortList.sort(allInOut , "time" , "desc");
 
-        Map resultMap = ResultUtil.success(allInOut);
         return resultMap;
     }
 
@@ -206,21 +220,28 @@ public class WithdrawController {
     @RequestMapping(value="/withdrawRecords" , method = RequestMethod.GET)
     @ResponseBody
     public Map<String , Object> withdrawRecords(String userId){
-        List<InOutDetailsPOJO> inOutDetailsPOJOs  = withdrawService.queryWithdrawRecord(userId);
-        List<WithdrawOutParam> withdrawOutParams = new  ArrayList<>();
-        for(InOutDetailsPOJO inOutDetailsPOJO : inOutDetailsPOJOs){
-            WithdrawOutParam withdrawOP = new WithdrawOutParam();
-            withdrawOP.setWithdrawName("提现");
-            DecimalFormat df = new DecimalFormat("#0.00");
-            withdrawOP.setPrice("-"+df.format(((double)inOutDetailsPOJO.getPrice())/100));
-            try {
-                withdrawOP.setTime(DateUtils.longToString(inOutDetailsPOJO.getTime(), "yyyy-MM-dd HH:mm:ss"));
-            } catch (ParseException e) {
-                e.printStackTrace();
+        Map<String , Object> resultMap = null;
+        if(userId != null && !"".equals(userId)){
+            String uId = userId.replace("\"","");
+            List<InOutDetailsPOJO> inOutDetailsPOJOs  = withdrawService.queryWithdrawRecord(uId);
+            List<WithdrawOutParam> withdrawOutParams = new  ArrayList<>();
+            for(InOutDetailsPOJO inOutDetailsPOJO : inOutDetailsPOJOs){
+                WithdrawOutParam withdrawOP = new WithdrawOutParam();
+                withdrawOP.setWithdrawName("提现");
+                DecimalFormat df = new DecimalFormat("#0.00");
+                withdrawOP.setPrice("-"+df.format(((double)inOutDetailsPOJO.getPrice())/100));
+                try {
+                    withdrawOP.setTime(DateUtils.longToString(inOutDetailsPOJO.getTime(), "yyyy-MM-dd HH:mm:ss"));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                withdrawOutParams.add(withdrawOP);
             }
-            withdrawOutParams.add(withdrawOP);
+            resultMap = ResultUtil.success(withdrawOutParams);
+        }else{
+            resultMap = ResultUtil.fail(1004,"用户Id不能为空");
         }
-        return ResultUtil.success(withdrawOutParams);
+        return resultMap;
     }
 
     /**
@@ -231,17 +252,24 @@ public class WithdrawController {
     @RequestMapping(value="/getRecordDetails" , method = RequestMethod.GET)
     @ResponseBody
     public Map<String , Object> getRecordList(String userId){
-        List<InOutDetailsPOJO> inOutDetailsPOJOs  = withdrawService.queryRecordDetails(userId);
-        List<InOutDetailsOutParam> incomeDetails = new ArrayList<>();
-        for(InOutDetailsPOJO inOutDetailsPOJO : inOutDetailsPOJOs ){
-            InOutDetailsOutParam inOutDetailsOutParam = new InOutDetailsOutParam();
-            inOutDetailsOutParam.setGoodsName(inOutDetailsPOJO.getGoodsName());//操作
-            inOutDetailsOutParam.setTime(inOutDetailsPOJO.getTime());//时间戳
-            DecimalFormat df = new DecimalFormat("#0.00");
-            inOutDetailsOutParam.setPrice(df.format(((double)inOutDetailsPOJO.getPrice())/100));//金额
-            incomeDetails.add(inOutDetailsOutParam);
+        Map<String , Object> resultMap = null;
+        if(userId != null && !"".equals(userId)){
+            String uId = userId.replace("\"","");
+            List<InOutDetailsPOJO> inOutDetailsPOJOs  = withdrawService.queryRecordDetails(uId);
+            List<InOutDetailsOutParam> incomeDetails = new ArrayList<>();
+            for(InOutDetailsPOJO inOutDetailsPOJO : inOutDetailsPOJOs ){
+                InOutDetailsOutParam inOutDetailsOutParam = new InOutDetailsOutParam();
+                inOutDetailsOutParam.setGoodsName(inOutDetailsPOJO.getGoodsName());//操作
+                inOutDetailsOutParam.setTime(inOutDetailsPOJO.getTime());//时间戳
+                DecimalFormat df = new DecimalFormat("#0.00");
+                inOutDetailsOutParam.setPrice(df.format(((double)inOutDetailsPOJO.getPrice())/100));//金额
+                incomeDetails.add(inOutDetailsOutParam);
+            }
+            resultMap = ResultUtil.success(incomeDetails);
+        }else{
+            resultMap = ResultUtil.fail(1004,"用户Id不能为空");
         }
-        return ResultUtil.success(incomeDetails);
+        return resultMap;
     }
 
     /**
@@ -252,19 +280,23 @@ public class WithdrawController {
     @RequestMapping(value="/getUnrecordDetails" , method = RequestMethod.GET)
     @ResponseBody
     public Map<String , Object> getUnrecordList(String userId){
-        List<InOutDetailsPOJO> inOutDetailsPOJOs  = withdrawService.queryUnrecordDetails(userId);
-        List<InOutDetailsOutParam> incomeDetails = new ArrayList<>();
-        for(InOutDetailsPOJO inOutDetailsPOJO : inOutDetailsPOJOs ){
-            InOutDetailsOutParam inOutDetailsOutParam = new InOutDetailsOutParam();
-            inOutDetailsOutParam.setGoodsName(inOutDetailsPOJO.getGoodsName());//操作
-            inOutDetailsOutParam.setTime(inOutDetailsPOJO.getTime());//时间戳
-            DecimalFormat df = new DecimalFormat("#0.00");
-            inOutDetailsOutParam.setPrice(df.format(((double)inOutDetailsPOJO.getPrice())/100));//金额
-            incomeDetails.add(inOutDetailsOutParam);
+        Map<String , Object> resultMap = null;
+        if(userId != null && !"".equals(userId)){
+            String uId = userId.replace("\"","");
+            List<InOutDetailsPOJO> inOutDetailsPOJOs  = withdrawService.queryUnrecordDetails(uId);
+            List<InOutDetailsOutParam> incomeDetails = new ArrayList<>();
+            for(InOutDetailsPOJO inOutDetailsPOJO : inOutDetailsPOJOs ){
+                InOutDetailsOutParam inOutDetailsOutParam = new InOutDetailsOutParam();
+                inOutDetailsOutParam.setGoodsName(inOutDetailsPOJO.getGoodsName());//操作
+                inOutDetailsOutParam.setTime(inOutDetailsPOJO.getTime());//时间戳
+                DecimalFormat df = new DecimalFormat("#0.00");
+                inOutDetailsOutParam.setPrice(df.format(((double)inOutDetailsPOJO.getPrice())/100));//金额
+                incomeDetails.add(inOutDetailsOutParam);
+            }
+            resultMap = ResultUtil.success(incomeDetails);
+        }else{
+            resultMap = ResultUtil.fail(1004,"用户Id不能为空");
         }
-        return ResultUtil.success(incomeDetails);
+        return resultMap;
     }
-
-
-
 }
