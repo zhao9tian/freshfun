2cabe183cc26dfca10c5619f1fc70c7e857712c6
Tu ZL <18721394619@163.com>
2016-10-07 15:32:00 +0800

    去除MessageUtils的warning,在申请退款时将订单状态改为40，并加上修改时间

--- src/main/java/com/quxin/freshfun/utils/MessageUtils.java
+++ src/main/java/com/quxin/freshfun/utils/MessageUtils.java
@@ -1,28 +1,26 @@
 package com.quxin.freshfun.utils;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
 
 /**
  * 短信验证码
- * @author TuZl
+ * @author qucheng
  * @time 2016年9月6日下午9:37:20
  */
 public class MessageUtils {
 	/**
 	 * 发送短信
 	 */
-	private static void sendMessage(String code , String phoneNum){
-//		String content = code+"登录验证(一分钟内有效)"; 
-		String content = "【FreshFun】"+"您的短信验证码"+code+"，请勿将验证码给他人使用。"; 
+	private static void sendMessage(String code , String phoneNum) throws IOException {
+		String content = "【FreshFun】"+"您的短信验证码"+code+"，请勿将验证码给他人使用。";
 		String sign="";
 		
 		// 创建StringBuffer对象用来操作字符串
-		StringBuffer sb = new StringBuffer("http://sms.1xinxi.cn/asmx/smsservice.aspx?");
+		StringBuffer sb;
+		sb = new StringBuffer("http://sms.1xinxi.cn/asmx/smsservice.aspx?");
 
 		// 向StringBuffer追加用户名
 		sb.append("name=quxin2016");
@@ -31,52 +29,47 @@ public class MessageUtils {
 		sb.append("&pwd=C877EC8499ACDA41BC44548C64AB");
 
 		// 向StringBuffer追加手机号码
-		sb.append("&mobile="+phoneNum);
+		sb.append("&mobile=").append(phoneNum);
 
 		// 向StringBuffer追加消息内容转URL标准码
-		try {
-			sb.append("&content="+URLEncoder.encode(content,"UTF-8"));
-			//追加发送时间，可为空，为空为及时发送
-			sb.append("&stime=");
-			
-			//加签名
-			sb.append("&sign="+URLEncoder.encode(sign,"UTF-8"));
-			//type为固定值pt  extno为扩展码，必须为数字 可为空
-			sb.append("&type=pt&extno=");
-			// 创建url对象
-			URL url = new URL(sb.toString());
-			
-			// 打开url连接
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			
-			// 设置url请求方式 ‘get’ 或者 ‘post’
-			connection.setRequestMethod("POST");
-			
-			// 发送
-			url.openStream();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (ProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		
+		sb.append("&content=").append(URLEncoder.encode(content, "UTF-8"));
+		//追加发送时间，可为空，为空为及时发送
+		sb.append("&stime=");
+
+		//加签名
+		sb.append("&sign=").append(URLEncoder.encode(sign, "UTF-8"));
+		//type为固定值pt  extno为扩展码，必须为数字 可为空
+		sb.append("&type=pt&extno=");
+		// 创建url对象
+		URL url = new URL(sb.toString());
+
+		// 打开url连接
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+		// 设置url请求方式 ‘get’ 或者 ‘post’
+		connection.setRequestMethod("POST");
+
+		// 发送
+		url.openStream();
+
 
 	}
 	
 	/**
 	 * 给手机号发送短信验证码
-	 * @param phoneNum
-	 * @return
+	 * @param phoneNum 手机号
+	 * @return 发送并返回验证码
 	 */
 	public static String createMessage(String phoneNum){
-		String code = "";
+		String code;
 		Integer i = (int) (Math.random()*9000+1000);
 		code = i.toString();
 		//发送短信
-		sendMessage(code , phoneNum);
+		try {
+			sendMessage(code , phoneNum);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return code;
 	}
 	
