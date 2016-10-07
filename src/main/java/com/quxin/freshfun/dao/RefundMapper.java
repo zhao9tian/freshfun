b628a2f6667f04f044cfd97c7e6c1844dfdec7eb
Tu ZL <18721394619@163.com>
2016-10-07 21:46:33 +0800

    1.去除退款warning 2.申请退款保留退款前的状态 3.退款列表显示退款给状态

--- src/main/java/com/quxin/freshfun/dao/RefundMapper.java
+++ src/main/java/com/quxin/freshfun/dao/RefundMapper.java
@@ -2,19 +2,21 @@ package com.quxin.freshfun.dao;
 
 import com.quxin.freshfun.model.RefundPOJO;
 
+import java.util.Map;
+
 public interface RefundMapper {
 
 	/**
 	 * 保存退款信息
-	 * @param refund
-	 * @return
+	 * @param refund 退款对象
+	 * @return 返回保存的条数
 	 */
 	Integer save(RefundPOJO refund);
 	
 	/**
 	 * 根据订单Id修改退款状态
-	 * @param orderDetailId
-	 * @return
+	 * @param map 订单id, 订单状态
+	 * @return 返回修改的数目
 	 */
-	Integer updateRefundStatus(String orderDetailId);
+	Integer updateRefundStatus(Map<String , Object> map);
 }
--- src/main/java/com/quxin/freshfun/service/impl/refund/RefundServiceImpl.java
+++ src/main/java/com/quxin/freshfun/service/impl/refund/RefundServiceImpl.java
@@ -1,5 +1,6 @@
 package com.quxin.freshfun.service.impl.refund;
 
+import com.quxin.freshfun.dao.OrderDetailsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,16 +8,28 @@ import com.quxin.freshfun.dao.RefundMapper;
 import com.quxin.freshfun.model.RefundPOJO;
 import com.quxin.freshfun.service.refund.RefundService;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service("refundService")
 public class RefundServiceImpl implements RefundService{
 
 	@Autowired
 	private RefundMapper refundMapper;
+	@Autowired
+	private OrderDetailsMapper orderDetailsMapper;
 	
 	@Override
 	public Integer save(RefundPOJO refund) {
+		//保存退款申请信息
 		refundMapper.save(refund);
-		refundMapper.updateRefundStatus(refund.getOrderDetailsId());
+		//修改订单状态为退货
+		//保存退款前的状态
+		Integer orderStatus = orderDetailsMapper.selectSigleOrder(refund.getOrderDetailsId()).get(0).getOrderStatus();
+		Map<String , Object> map = new HashMap<>();
+		map.put("orderId" ,refund.getOrderDetailsId());
+		map.put("orderStatus" , orderStatus);
+		refundMapper.updateRefundStatus(map);
 		return refund.getId();
 	}
 
