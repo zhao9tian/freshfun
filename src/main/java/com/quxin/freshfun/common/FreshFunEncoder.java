fd2bcb43dcf4190be7d43dcf8fb587f8d63541d0
tianmingzhao <83247083@qq.com>
2016-10-12 15:33:56 +0800

    1、controller 异常拦截器 2、ID加密算法

---
+++ src/main/java/com/quxin/freshfun/common/FreshFunEncoder.java
@@ -0,0 +1,32 @@
+package com.quxin.freshfun.common;
+
+/**
+ * Created by tianmingzhao on 16/10/12.
+ */
+class FreshFunEncoder {
+
+    /**
+     * ID 加密
+     * @param id
+     * @return
+     */
+    public static String idToUrl(long id) {
+        return 1 + Long.toString(id * 3L + 51L, 29);
+    }
+
+    /**
+     * ID 解密
+     * @param url
+     * @return
+     */
+    public static long urlToId(String url) {
+        return (Long.parseLong(url.substring(1), 29) - 51L) / 3L;
+    }
+
+//    public static void main(String[] args) {
+//        Long userId = 1123l;
+//        String key = idToUrl(userId);
+//        System.out.println(key);
+//        System.out.println(urlToId(key));
+//    }
+}
---
+++ src/main/java/com/quxin/freshfun/service/aop/RequestLogAspect.java
@@ -0,0 +1,68 @@
+package com.quxin.freshfun.service.aop;
+
+import com.alibaba.fastjson.JSON;
+import com.quxin.freshfun.utils.ResultUtil;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 记录请求日志的切面,记录请求参数、返回结果以及处理耗时
+ * Created by baoluo on 15/12/7.
+ */
+@Aspect
+@Service
+public class RequestLogAspect {
+
+    private static Logger logger = LoggerFactory.getLogger(RequestLogAspect.class);
+
+    @Around("execution(public * com.quxin.freshfun.controller.*.*.*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object[] args = joinPoint.getArgs();
+
+        String resultString = null;
+
+        try{
+            preLog(joinPoint.toString(),args);
+            Object ret = joinPoint.proceed();
+            return ret;
+        } catch (Throwable t){
+            logger.error("系统异常" ,t);
+            return ResultUtil.fail(4004,"系统异常");
+        } finally {
+            long cost = System.currentTimeMillis() - start;
+            afterLog(joinPoint.toString(), resultString, cost);
+        }
+    }
+
+
+    private void  preLog(String method,Object[] args){
+        StringBuilder paramString = new StringBuilder();
+        if (args != null) {
+            for (Object obj : args) {
+                paramString.append(JSON.toJSONString(obj)).append(",");
+            }
+        }
+
+
+        StringBuilder sb = new StringBuilder(200);
+        sb.append("method:").append(method);
+        sb.append(" ").append("parameter:").append(paramString);
+        sb.append(" ").append("start");
+        logger.info(sb.toString());
+    }
+
+    private void  afterLog(String method, String result, long cost){
+
+        StringBuilder sb = new StringBuilder(200);
+        sb.append("method:").append(method);
+        sb.append(" ").append("result:").append(result);
+        sb.append(" ").append("cost:").append(cost).append("ms");
+        sb.append(" ").append("end");
+        logger.info(sb.toString());
+    }
+}
